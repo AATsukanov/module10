2b1ecb3d02e12511a12c58d823008c00ff57c958
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-08-21 14:59:54 +0300

    тест состояния гонки

---
+++ module_10_Practice_race_condition.py
@@ -0,0 +1,24 @@
+import threading
+
+''' В версии python выше 3.10
+    эта проблема решена!
+'''
+
+x = 0
+
+def thread_task(n):
+    global x
+    for _ in range(n):
+        x = x + 1
+
+def main():
+    t1 = threading.Thread(target=thread_task, args=(1_000_000,))
+    t2 = threading.Thread(target=thread_task, args=(1_000_000,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print(x)
+
+if __name__ == '__main__':
+    main()
