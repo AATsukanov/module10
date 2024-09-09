8f81d155eb23b83c6f0c14beb8b6917a3ff621a6
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-09-09 15:22:02 +0300

    Добавлено практическое занятие 10_practice (multiprocessing, mp.Process, mp.Queue, pillow)

---
+++ module_10_practice.py
@@ -0,0 +1,37 @@
+import multiprocessing as mp
+from PIL import Image # pip install pillow
+from queue import Empty
+import os
+
+def resize_image(fnames, queue):
+    for fname in fnames:
+        img = Image.open(fname)
+        img = img.resize((640, 360)) #кортеж в аргументе (xr, yr)
+        queue.put((fname, img))
+
+def change_color_and_save(queue):
+    while True:
+        try:
+            fname, img = queue.get(timeout=5) #после опустошения ждем 5 секунд и вырубаемся
+        except Empty:
+            break
+        img = img.convert('L') #L - черно-белый
+        img.save(fname[:-3] + 'bw.png')
+
+if __name__ == '__main__':
+    data = []
+    queue = mp.Queue()
+
+    #for image in range(1, 10):
+    #    data.append(f'./images/img_{image}.jpg')
+    data = ['./images/'+fname for fname in os.listdir('./images/') if '.png' in fname]
+
+
+    resize_process = mp.Process(target=resize_image, args=(data, queue))
+    change_process = mp.Process(target=change_color_and_save, args=(queue, )) #кортеж на входе поэтому (.., )
+
+    resize_process.start()
+    change_process.start()
+
+    resize_process.join()
+    change_process.join()
